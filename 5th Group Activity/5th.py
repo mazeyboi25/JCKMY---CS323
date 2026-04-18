5eddf7cfd14f7d64099d43dfd42b98d2a568d204
Cielo Salas <cielosalas141516@gmail.com>
2026-04-18 21:47:36 +0800

    Created search and parallel code

--- 5th Group Activity/5th.py
+++ 5th Group Activity/5th.py
@@ -2,7 +2,7 @@ import random
 import time
 from multiprocessing import Process, Queue
 
-# Dataset generator
+# Dataset 
 def make_data(n):
     data = []
     for _ in range(n):
@@ -40,3 +40,73 @@ def merge_sort(arr):
         merged += right[j:]
 
     return merged
+
+
+    # ---------------- SEARCH (SEQUENTIAL) ----------------
+def linear_search(arr, target):
+    idx = -1
+    for i in range(len(arr)):
+        if arr[i] == target:
+            idx = i
+            break
+    return idx
+
+
+# ---------------- PARALLEL SORT ----------------
+def parallel_merge_sort(data):
+    # split into 4 parts
+    size = len(data)
+    step = size // 4
+
+    chunks = []
+    start = 0
+    for i in range(4):
+        if i == 3:
+            chunks.append(data[start:])  # last part gets the extra items
+        else:
+            chunks.append(data[start:start + step])
+        start += step
+
+    q = Queue()
+
+    def worker(sub):
+        sorted_part = merge_sort(sub)
+        q.put(sorted_part)
+
+    procs = []
+    for c in chunks:
+        p = Process(target=worker, args=(c,))
+        procs.append(p)
+        p.start()
+
+    results = []
+    for _ in procs:
+        results.append(q.get())
+
+    for p in procs:
+        p.join()
+
+    # merge the sorted parts together
+    result = results[0]
+    for r in results[1:]:
+        result = merge(result, r)
+
+    return result
+
+
+def merge(a, b):
+    res = []
+    i = j = 0
+
+    while i < len(a) and j < len(b):
+        if a[i] < b[j]:
+            res.append(a[i]); i += 1
+        else:
+            res.append(b[j]); j += 1
+
+    if i < len(a):
+        res.extend(a[i:])
+    if j < len(b):
+        res.extend(b[j:])
+
+    return res
